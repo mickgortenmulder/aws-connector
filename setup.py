5794527b73adfd89097dd1d8d5cc00c97890d12c
Mick Gortenmulder <mickgortenmulder@gmail.com>
2022-10-06 08:49:17 +0200

    versioning

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="aws-connector",
-    version="0.0.6",
+    version="0.1.0",
     author="Mick Gortenmulder",
     author_email="mickgortenmulder@gmail.com",
     description="AWS connector",
