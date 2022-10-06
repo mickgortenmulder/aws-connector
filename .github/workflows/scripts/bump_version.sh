c6ccbef688cb4e9c892086e42524acef9fbdf3a9
Mick Gortenmulder <mickgortenmulder@gmail.com>
2022-10-06 14:24:30 +0200

    (fix) Code sanity updates

--- .github/workflows/scripts/bump_version.sh
+++ .github/workflows/scripts/bump_version.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-new_version=`grep version pyproject.toml | awk '{print $3}'`
+old_version=`grep version pyproject.toml | awk '{print $3}'`
 commit_message=`git log -1 --format=%B`
+
 echo $commit_message
 echo $new_version
 echo $env
@@ -11,11 +12,13 @@ echo $ref_name
 echo $event
 echo $pr_title
 
-
 if [[ "$commit_message" == *"fix"* ]]; then
+  bump2version --current-version $old_version patch pyproject.toml
   echo "Patch."
 elif [[ "$commit_message" == *"feat"* ]]; then
+  bump2version --current-version $old_version minor pyproject.toml
   echo "Minor."
 elif [[ "$commit_message" == *"BREAKING CHANGE"* ]]; then
+  bump2version --current-version $old_version major pyproject.toml
   echo "Major."
 fi
