846cf0da748d442435fa3ff4bf22d887d9ae6d8c
Mick Gortenmulder <mickgortenmulder@gmail.com>
2022-10-06 15:47:20 +0200

    (fix) Test bumpversion

--- .github/workflows/scripts/bump_version.sh
+++ .github/workflows/scripts/bump_version.sh
@@ -1,51 +1,36 @@
 #!/usr/bin/env bash
-
 old_version=`grep version pyproject.toml | awk '{print $3}'`
-# commit_message=`git log -1 --format=%B`
-
-echo $message
-echo $old_version
-# echo $commit_message
-echo $new_version
-echo $env
-echo $repository
-echo $repository_owner
-echo $ref_name
-echo $event
-echo $pr_title
+echo "Commit message: $message"
+echo "Old package version: $old_version"
+echo "New package version: $new_version"
+echo "Env: $env"
 
 git config --global user.email "mickgortenmulder@gmail.com"
 git config --global user.name "Mick Gortenmulder"
 git add . ; git commit -m 'Commit changes'
 
 if [[ "$message" == *"fix"* ]]; then
-  echo "Patch."
+  echo "Patch update."
   bump2version --current-version $old_version patch pyproject.toml
-  new_version=`grep version pyproject.toml | awk '{print $3}'`
-  echo $new_version
 
 elif [[ "$message" == *"feat"* ]]; then
-  echo "Minor."
+  echo "Minor update."
   bump2version --current-version $old_version minor pyproject.toml
-  new_version=`grep version pyproject.toml | awk '{print $3}'`
-  echo $new_version
 
 elif [[ "$message" == *"BREAKING CHANGE"* ]]; then
-  echo "Major."
+  echo "Major update."
   bump2version --current-version $old_version major pyproject.toml
-  new_version=`grep version pyproject.toml | awk '{print $3}'`
-  echo $new_version
 
 else
   echo "No matches"
 fi
 
 if [ "$old_version" != "$new_version" ]; then
-  git branch -v
-  git log -1 HEAD
-  git log -1 HEAD^
-  git status
+  new_version=`grep version pyproject.toml | awk '{print $3}'`
+  echo $new_version
   git add pyproject.toml
   git commit -m "(fix) bump version to $new_version"
   git push origin $ref_name
+else
+  echo "Nothing to update"
 fi
