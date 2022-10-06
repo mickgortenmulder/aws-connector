124db2ca4d545d360a04bd802766ade3441723cc
Mick Gortenmulder <mickgortenmulder@gmail.com>
2022-10-06 13:59:22 +0200

    (fix) Code sanity updates

--- .github/workflows/scripts/bump_version.sh
+++ .github/workflows/scripts/bump_version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 new_version=`grep version pyproject.toml | awk '{print $3}'`
-commit_message=`git log HEAD^..HEAD --oneline --decorate`
+commit_message=`git log -1`
 echo $commit_message
 echo $new_version
 echo $env
