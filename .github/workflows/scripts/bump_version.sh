941f5348ed421fdc4ad19d24ef6b51c10f2aa38d
Mick Gortenmulder <mickgortenmulder@gmail.com>
2022-10-07 09:12:02 +0200

    (bla): Version and release update

--- .github/workflows/scripts/bump_version.sh
+++ .github/workflows/scripts/bump_version.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 old_version=`grep version pyproject.toml | awk '{print $3}'`
-echo "Commit message: $message"
 echo "Old package version: $old_version"
 
 git config --global user.email "mickgortenmulder@gmail.com"
@@ -8,15 +7,15 @@ git config --global user.name "Mick Gortenmulder"
 git add . ; git commit -m 'Commit changes'
 
 if [[ "$message" == *"fix"* ]]; then
-  echo "This change is considered a patch package update based on the commit message."
+  echo "This change is considered a patch package update based on the commit message: $message"
   bump2version --current-version $old_version patch pyproject.toml
 
 elif [[ "$message" == *"feat"* ]]; then
-  echo "This change is considered a minor package update based on the commit message."
+  echo "This change is considered a minor package update based on the commit message: $message"
   bump2version --current-version $old_version minor pyproject.toml
 
 elif [[ "$message" == *"BREAKING CHANGE"* ]]; then
-  echo "This change is considered a major package update based on the commit message."
+  echo "This change is considered a major package update based on the commit message: $message"
   bump2version --current-version $old_version major pyproject.toml
 
 else
@@ -32,4 +31,5 @@ if [ "$old_version" != "$new_version" ]; then
   git push origin $ref_name
 else
   echo "Nothing to update"
+  exit 1
 fi
